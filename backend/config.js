fee25e88e8d224e07012526084a4acd28294925f
Yash <yash27560@gmail.com>
2025-04-03 11:58:36 +0530

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT=5555;
+export const mongoDBURL = 'mongodb+srv://yash27560:VU0wAyLj5dJFdH7d@book-store-mern.5y7z5k9.mongodb.net/books-collection?retryWrites=true&w=majority'
---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express, { request, response } from "express";
+import {PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
+const app = express();
+
+app.post('/books', async(request,response)=>{
+    try{
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message:'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook={
+            title:request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book= await Book.create(newBook);
+        return response.status(201).send(book);
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+mongoose 
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log('App connected to database');
+        app.listen(PORT, ()=>{
+            console.log(`App is listening to port: ${PORT}`);
+        });
+        
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+    
