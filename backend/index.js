7b28cc4b00f673d4e3b2bd7c8cfa814e869a4940
Yash <yash27560@gmail.com>
2025-04-03 16:01:37 +0530

    Added CORS Policy

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,19 @@ import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 //Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handling CORS Policy
+app.use(cors());
 
 app.get('/',(request,response)=>{
     console.log(request);
-    return response.status(234).send('Welcome to MERN Stack Tutorial')
+    return response.status(200).send('Welcome to MERN Stack Tutorial')
 });
 
 app.use('/books',booksRoute);
