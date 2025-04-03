1f28e00931ba11528d174c164b041aaf360635de
Yash <yash27560@gmail.com>
2025-04-03 15:25:39 +0530

    Added functionality to fetching one book or all books, updating a book and deleting a book

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ app.get('/',(request,response)=>{
     return response.status(234).send('Welcome to MERN Stack Tutorial')
 });
 
+//Route for Add Book to Database
 app.post('/books', async(request,response)=>{
     try{
         if (
@@ -34,6 +35,77 @@ app.post('/books', async(request,response)=>{
         console.log(error.message);
         response.status(500).send({message:error.message});
     }
+});
+
+//Route for Get All Books from Database
+app.get('/books', async (request,response) => {
+    try{
+        const books =await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+//Route for Get One Book from Database by ID
+app.get('/books/:id', async (request,response) => {
+    try{
+        const { id }=request.params;
+
+        const book =await Book.findById(id);
+        return response.status(200).json(book);
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+//Route for DELETE A BOOK
+app.delete('/books/:id',async(request,response)=>{
+    try{
+        const {id} =request.params;
+        const result=await Book.findByIdAndUpdate(id);
+
+        if (!result){
+            return response.status(404).json({message: 'Book Not Found'});
+        }
+        else{
+            return response.status(200).send({message: "Book Succesfully Deleted"});
+        }
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Route for Update a Book
+app.put('/books/:id', async (request,response) => {
+    try{
+        if(
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send All Required Fields: title, author, publishYear',
+            });
+        }
+
+        const { id }=request.params;
+        const result=await Book.findByIdAndUpdate(id, request.body);
+        if(!result){
+            return response.status(404).json({message: 'Book Not Found'});
+        }
+
+        return response.status(200).send({message: 'Book Updated Successfully'});
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }    
 });
 
 mongoose 
