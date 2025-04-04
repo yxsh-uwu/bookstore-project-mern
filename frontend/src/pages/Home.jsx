18ef665c4befbde3660c92134fe73bc600858eaa
Yash <yash27560@gmail.com>
2025-04-04 13:06:05 +0530

    Fixed incorrect id at home page leading to broken showbook functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -65,13 +65,13 @@ const Home = () => {
                                 </td>
                                 <td className='border border-slate-700 rounded-md text-center'>
                                     <div className='flex justify-center gap-x-4'>
-                                        <Link to={`/books/details/${books._id}`}>
+                                        <Link to={`/books/details/${book._id}`}>
                                             <BsInfoCircle className='text-2xl text-green-800' />
                                         </Link>
-                                        <Link to={`/books/edit/${books._id}`}>
+                                        <Link to={`/books/edit/${book._id}`}>
                                             <AiOutlineEdit className='text-2xl text-yellow-600' />
                                         </Link>
-                                        <Link to={`/books/delete/${books._id}`}>
+                                        <Link to={`/books/delete/${book._id}`}>
                                             <MdOutlineDelete className='text-2xl text-red-600' />
                                         </Link>
 
